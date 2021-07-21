6d6fd42a69ffb994eddb1b33767f22fc7f7c56fc
dhiren <dhiren474dp@gmail.comgit config --global user.email dhiren474dp@gmail.com>
2021-07-21 11:23:37 +0530

    file added

---
+++ script.js
@@ -0,0 +1,14 @@
+var css = document.querySelector("h3");
+var col1= document.querySelector(".color1");
+var col2= document.querySelector(".color2");
+var body= document.getElementById("gradient");
+
+function colorpick(){
+	body.style.background="linear-gradient(to right,"+ col1.value+","+ col2.value+")";
+
+	css.textContent=body.style.background+";";
+}
+
+col1.addEventListener("input",colorpick)
+
+col2.addEventListener("input",colorpick)
